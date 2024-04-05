b8aa38e3209e4d1239381e1f977fbbd6aa6a7294
Himanshu Borole <himanshu.borole@gmail.com>
2024-04-05 09:01:29 -0700

    Updated to include translation

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM public.ecr.aws/lambda/python:3.10
 
 # Copy requirements.txt
-COPY requirements.txt  ${LAMBDA_TASK_ROOT}
+COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
 # Copy your lambda function code into the container
-COPY src/* ${LAMBDA_TASK_ROOT}
+COPY src/* .
 
 RUN yum -y install git wget tar xz
 RUN wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz && tar xvf ffmpeg-release-amd64-static.tar.xz && mv ffmpeg-6.1-amd64-static/ffmpeg /usr/bin/ffmpeg && rm -Rf ffmpeg*
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,8 +6,9 @@ import json
 import boto3
 import logging
 import whisper
-import torch
 import os
+import json
+from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
 # Set up logging
 logger = logging.getLogger()
@@ -15,70 +16,203 @@ logger.setLevel(logging.INFO)
 
 # Set up S3 client
 s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+
+# Set up the languages
+
+languages = {
+    "Arabic": "ar_AR",
+    "Czech": "cs_CZ",
+    "German": "de_DE",
+    "English": "en_XX",
+    "Spanish": "es_XX",
+    "Estonian": "et_EE",
+    "Finnish": "fi_FI",
+    "French": "fr_XX",
+    "Gujarati": "gu_IN",
+    "Hindi": "hi_IN",
+    "Italian": "it_IT",
+    "Japanese": "ja_XX",
+    "Kazakh": "kk_KZ",
+    "Korean": "ko_KR",
+    "Lithuanian": "lt_LT",
+    "Latvian": "lv_LV",
+    "Burmese": "my_MM",
+    "Nepali": "ne_NP",
+    "Dutch": "nl_XX",
+    "Romanian": "ro_RO",
+    "Russian": "ru_RU",
+    "Sinhala": "si_LK",
+    "Turkish": "tr_TR",
+    "Vietnamese": "vi_VN",
+    "Chinese": "zh_CN",
+    "Afrikaans": "af_ZA",
+    "Azerbaijani": "az_AZ",
+    "Bengali": "bn_IN",
+    "Persian": "fa_IR",
+    "Hebrew": "he_IL",
+    "Croatian": "hr_HR",
+    "Indonesian": "id_ID",
+    "Georgian": "ka_GE",
+    "Khmer": "km_KH",
+    "Macedonian": "mk_MK",
+    "Malayalam": "ml_IN",
+    "Mongolian": "mn_MN",
+    "Marathi": "mr_IN",
+    "Polish": "pl_PL",
+    "Pashto": "ps_AF",
+    "Portuguese": "pt_XX",
+    "Swedish": "sv_SE",
+    "Swahili": "sw_KE",
+    "Tamil": "ta_IN",
+    "Telugu": "te_IN",
+    "Thai": "th_TH",
+    "Tagalog": "tl_XX",
+    "Ukrainian": "uk_UA",
+    "Urdu": "ur_PK",
+    "Xhosa": "xh_ZA",
+    "Galician": "gl_ES",
+    "Slovene": "sl_SI",
+}
 
 def handler(event, context):
+    logger.info(f'Event: {event}')
     try:
-        # Check if NVIDIA GPU is available
-        torch.cuda.is_available()
-        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-        logger.info(f'Device: {DEVICE}')
-
-        # Model is already present in root
-        model = whisper.load_model('./small.pt', device=DEVICE)
-        
-        logger.info(f'Model loaded successfully')
-
-        # Get the bucket from the lambda environment variables
-        bucket = os.environ['BUCKET_NAME']
-
         # Get the key from the event
-        key = json.loads(event['body'])['key']
+        event_body = json.loads(event['body'])
+        key = event_body['key']
+        is_translate = event_body.get('is_translate', 'false')
+        translate_to = event_body.get('translate_to', 'English')
+        transcript = event_body.get('transcript', '')
         
-        # Download the file from S3
-        local_file = '/tmp/' + key
-        s3.download_file(bucket, key, local_file)
+        if is_translate == 'true':
+            # Check if the language code is valid
+            logger.info(f'Translating to: {translate_to}')
+            if translate_to not in languages:
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps({
+                        'message': 'Invalid language code',
+                        'error': f'{translate_to} is not a valid language code'
+                    })
+                }
 
-        # Log the file download
-        logger.info(f'File downloaded from S3: {local_file}')
+            if transcript == '':
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps({
+                        'message': 'English Transcript not provided',
+                        'error': 'English Transcript is required for translation'
+                    })
+                }
 
-        audio = whisper.load_audio(local_file)
-        audio = whisper.pad_or_trim(audio)
+            logger.info(f'Translating text to {translate_to}')
 
-        logger.info(f'Audio loaded successfully')
+            # Load the model and tokenizer
+            model = MBartForConditionalGeneration.from_pretrained("SnypzZz/Llama2-13b-Language-translate")
+            tokenizer = MBart50TokenizerFast.from_pretrained("SnypzZz/Llama2-13b-Language-translate", src_lang="en_XX")
 
-        # make log-Mel spectrogram and move to the same device as the model
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+            english_text = json.loads(transcript)['english']
+            model_inputs = tokenizer(english_text, return_tensors="pt")
 
-        # detect the spoken language
-        _, probs = model.detect_language(mel)
-        detected_language = max(probs, key=probs.get)
-        print(f"Detected language: {detected_language}")
+            # translate from English to language
+            generated_tokens = model.generate(
+                **model_inputs,
+                forced_bos_token_id=tokenizer.lang_code_to_id[languages[translate_to]]
+            )
+            translation = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)            
+            logger.info(f'Translation to {translate_to}: {translation[0]}')
 
-        # decode the audio
-        options = whisper.DecodingOptions(fp16=False)
-        original = whisper.decode(model, mel, options)
-        result = [{'language': detected_language, 'transcript': original.text}]
+            result = { 'language': translate_to, 'transcript': translation[0] }
+            
+        else:
+            # Model is already present in root
+            model = whisper.load_model('./small.pt', device='cpu')
+            # model = whisper.load_model('./base.pt', device='cpu')
+            
+            logger.info(f'Small model loaded successfully')
+            
+            # Get the bucket from the lambda environment variables
+            bucket = os.environ['BUCKET_NAME']
+            
+            # Download the file from S3
+            local_file = '/tmp/' + key
+            s3.download_file(bucket, key, local_file)
 
-        # if the language is not English, translate the text
-        if detected_language != 'en':
-            translate = model.transcribe(local_file, task = "translate", fp16 = False)
-            result.append({'language': 'en', 'transcript': translate['text']})
-        
-        # Log the transcript
-        logger.info(f'Transcription: {result}')
+            # Log the file download
+            logger.info(f'File downloaded from S3: {local_file}')
 
-        # Upload the json file to S3
-        txt_file = '/tmp/' + key + '.txt'
-        with open(txt_file, 'w') as f:
-            json.dump(result, f)
-        s3.upload_file(txt_file, bucket, key + '.txt')
-        logger.info(f'Transcription uploaded to S3: {txt_file}')
+            audio = whisper.load_audio(local_file)
+            audio = whisper.pad_or_trim(audio)
+
+            logger.info(f'Audio loaded successfully')
+
+            # make log-Mel spectrogram and move to the same device as the model
+            mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+            # detect the spoken language
+            _, probs = model.detect_language(mel)
+            detected_language = max(probs, key=probs.get)
+            logger.info(f"Detected language: {detected_language}")
+
+            # decode the audio
+            options = whisper.DecodingOptions(fp16=False)
+            original = whisper.decode(model, mel, options)
+
+            if detected_language != 'en':
+                # Translate the audio to English
+                translate = model.transcribe(local_file, task="translate", fp16=False)
+
+            result = { 'language': detected_language, 'transcript': original.text, 'english': translate['text'] if detected_language != 'en' else original.text }
+
+            # Log the transcript
+            logger.info(f'Transcription: {result}')        
+
+        # Update the dynamodb table with the transcript
+        table = dynamodb.Table('lambdaWhisper')
+
+        try:
+            if is_translate == 'true':
+                # Update a single field in the database
+                response = table.update_item(
+                    Key={
+                        'key': key
+                    },
+                    UpdateExpression="set statusCode = :s, translated = :tr",
+                    ExpressionAttributeValues={
+                        ':s': 'COMPLETE',
+                        ':tr': result
+                    },
+                    ReturnValues="ALL_NEW",
+                )
+                
+                logger.info(f"Translation added to dynamodb: {response}")
+            else:
+                response = table.put_item(
+                    Item={
+                        'key': key,
+                        'statusCode': 'COMPLETE',
+                        'transcript': result
+                    }
+                )
+                logger.info(f"Transcript added to dynamodb: {response}")
+
+        except Exception as e:
+            logger.error(f"Error adding transcript / translate to dynamodb: {str(e)}")
+            logger.error(traceback.format_exc())
+            return {
+                'statusCode': 500,
+                'body': json.dumps({
+                    'message': 'Error occurred in the Lambda function',
+                    'error': str(e),
+                })
+            }
 
         # Return a json response
         return {
             'statusCode': 200,
             'body': json.dumps({
-                'message': 'Transcription completed successfully',
+                'message': 'Transcription/ translation completed successfully',
                 'result': result
             })
         }
